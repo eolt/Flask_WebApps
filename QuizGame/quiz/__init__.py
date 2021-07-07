6776cc287ca7382f6442196d47332952b1ce92bd
Edgar Olvera <eols7767@gmail.com>
2021-07-07 18:49:00 -0500

    Update __init__.py

--- QuizGame/quiz/__init__.py
+++ QuizGame/quiz/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask  # $ export FLASK_APP=hello   export FLASK_ENV=development
 
 app = Flask(__name__)
+# secret key given using terminal command: os.urandom(12).hex()
 app.config['SECRET_KEY'] = '78414504bd9a44d4de071e39'
 
 from quiz import routes
