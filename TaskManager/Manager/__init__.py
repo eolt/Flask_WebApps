4cdacb8efcba71cad9bf5a5521e751f4f0159493
Edgar Olvera <eols7767@gmail.com>
2021-07-07 18:58:43 -0500

    Update __init__.py

--- TaskManager/Manager/__init__.py
+++ TaskManager/Manager/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manager.db'
+# secret key given using terminal command: os.urandom(12).hex()
 app.config['SECRET_KEY'] = '78414504bd9a44d4de071e39'
 db = SQLAlchemy(app)
 
