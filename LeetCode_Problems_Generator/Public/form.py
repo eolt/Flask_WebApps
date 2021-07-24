9f60cd2b9952cbaf6bb3d252295a2cf650d7545d
Edgar Olvera <eols7767@gmail.com>
2021-07-23 23:26:31 -0500

    Delete LeetCode_Problems_Generator directory

--- LeetCode_Problems_Generator/Public/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-
-class SigninForm(FlaskForm):
-    firstName = StringField(label="First Name:", validators=[DataRequired()])
-    email = StringField(label="Email Address:", validators=[Email(), DataRequired()])
-    submit = SubmitField(label="Submit")
--- LeetCode_Problems_Generator/run.py
+++
@@ -1,4 +0,0 @@
-from Public import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
